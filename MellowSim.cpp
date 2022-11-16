e982a0ce46c237cacc92d93d116ea1c2203a0c6d
Severin Hotz <severin2448@gmail.com>
2022-11-16 14:06:05 +0100

    Remove OpenCV test method

--- MellowSim.cpp
+++ MellowSim.cpp
@@ -254,22 +254,3 @@ int main() {
 
     return 0;
 }
-
-
-//#include <iostream>
-//using namespace cv;
-//using namespace std;
-//int main(int argc, char** argv)
-//{
-//    Mat image;
-//    image = imread(argv[1], IMREAD_COLOR); // Read the file
-//    if (image.empty()) // Check for invalid input
-//    {
-//        cout << "Could not open or find the image" << std::endl;
-//        return -1;
-//    }
-//    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-//    imshow("Display window", image); // Show our image inside it.
-//    waitKey(0); // Wait for a keystroke in the window
-//    return 0;
-//}
