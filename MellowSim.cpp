db1ed047492480ea89d8255ed42388fbcc069c0b
Severin48 <severin2448@gmail.com>
2023-05-04 09:37:36 +0200

    More device info of GPUs/APUs

--- MellowSim.cpp
+++ MellowSim.cpp
@@ -166,7 +166,7 @@ void onClick(int event, int x, int y, int z, void*) {
 }
 
 int startKernel() {
-    printf("started running\n");
+    printf("Started running\n");
 
     // Create the two input vectors
     int i;
@@ -191,7 +191,7 @@ int startKernel() {
     source_str = (char*)malloc(MAX_SOURCE_SIZE);
     source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
     fclose(fp);
-    printf("kernel loading done\n");
+    printf("Kernel loading done\n\n");
     // Get platform and device information
     cl_device_id device_id = NULL;
     cl_uint ret_num_devices;
@@ -203,28 +203,43 @@ int startKernel() {
     platforms = (cl_platform_id*)malloc(ret_num_platforms * sizeof(cl_platform_id));
 
     ret = clGetPlatformIDs(ret_num_platforms, platforms, NULL);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     ret = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_ALL, 1, &device_id, &ret_num_devices);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
+
+    // MaxComputeUnits * MaxClockFrequency
+    cl_uint comp_units;
+    ret = clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_ulong), &comp_units, NULL);
+    cout << "MaxComputeUnits: " << comp_units << endl;
+
+    cl_uint clock_freq;
+    ret = clGetDeviceInfo(device_id, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(cl_ulong), &clock_freq, NULL);
+    cout << "MaxClockFrequency: " << clock_freq << endl;
+
+    cout << "MaxComputeUnits * MaxClockFrequency: " << comp_units * clock_freq << endl;
 
     cl_ulong mem_size;
     ret = clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &mem_size, NULL);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
     cout << "Mem size: " << mem_size << endl;
     
     char* device_name = new char[255];
     ret = clGetDeviceInfo(device_id, CL_DEVICE_NAME, sizeof(string), device_name, NULL);
-    printf("ret at %d is %d\n", __LINE__, ret);
     cout << "Device name: " << device_name << endl;
+
+    cl_bool available;
+    ret = clGetDeviceInfo(device_id, CL_DEVICE_AVAILABLE, sizeof(cl_bool), &available, NULL);
+    string available_str = available ? "Yes" : "No";
+    cout << "Device available: " << available_str << endl;
 
     // Create an OpenCL context
     cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     // Create a command queue
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     // Create memory buffers on the device for each vector 
     cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
@@ -237,36 +252,36 @@ int startKernel() {
     // Copy the lists A and B to their respective memory buffers
     ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
         LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0,
         LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
-    printf("before building\n");
+    //printf("before building\n");
     // Create a program from the kernel source
     cl_program program = clCreateProgramWithSource(context, 1,
         (const char**)&source_str, (const size_t*)&source_size, &ret);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     // Build the program
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
-    printf("after building\n");
+    //printf("after building\n");
     // Create the OpenCL kernel
     cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     // Set the arguments of the kernel
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&a_mem_obj);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&b_mem_obj);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&c_mem_obj);
-    printf("ret at %d is %d\n", __LINE__, ret);
+    //printf("ret at %d is %d\n", __LINE__, ret);
 
     //added this to fix garbage output problem
     //ret = clSetKernelArg(kernel, 3, sizeof(int), &LIST_SIZE);
@@ -302,7 +317,7 @@ int startKernel() {
     //free(C);
     free(platforms);
     free(source_str);
-    delete device_name;
+    delete [] device_name;
     return 0;
 }
 
